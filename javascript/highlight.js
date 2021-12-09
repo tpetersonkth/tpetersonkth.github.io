18ac1acf22ac7d8514488a1ea8b504059c24e921
Thomas Peterson <thomas_pierre_elis@hotmail.com>
2021-12-09 17:46:26 +0100

    Add code highlight through javascript

---
+++ javascript/highlight.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+	const preBlocks = document.getElementsByTagName("pre");
+	const len = preBlocks.length;
+	for (let i = 0; i < len; i++) {
+		console.log(i);
+		var block = document.getElementsByTagName("pre")[i];
+
+		while (block.innerHTML.indexOf("@@@") !== -1){
+			block.innerHTML = block.innerHTML.replace("@@@","<span class=\"highlight-attention\">").replace("@@@","</span>");
+		}
+
+		while (block.innerHTML.indexOf("@@") !== -1){
+			block.innerHTML = block.innerHTML.replace("@@","<span class=\"highlight-input\">").replace("@@","</span>");
+		}
+	}
+};
